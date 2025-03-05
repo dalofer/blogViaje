ad0255b351a833f01f9151559c4fad61eddd5849
dalofer <dalofer.ciber@gmail.com>
2025-03-05 08:24:47 +0100

    subida vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+    output: "standalone", // 💡 Optimiza el despliegue en Vercel
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import ClienteComponente from "@/app/components/ClienteComponente"; // 👈 Este sí es cliente
 import MainPage from "@/pages/itinerario"; // 👈 Este sí es cliente
 
 export default function Home() {
